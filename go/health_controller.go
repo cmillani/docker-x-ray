fae5178ce7db91873f2612e688fa2604a07c79a7
Carlos Eduardo Millani <carlos.millani@ambevtech.com.br>
2023-01-01 14:22:00 -0300

    feat: returning hostname as an example

--- go/health_controller.go
+++ go/health_controller.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"net/http"
+	"os"
 )
 
 func getHealthEndpoint(w http.ResponseWriter, req *http.Request) {
-	health := Health{Status: true}
+	name, err := os.Hostname()
+	status := true
+	if err != nil {
+		status = false
+	}
+	health := Health{Status: status, Host: name}
 	ResponseJson(w, health.status(), health)
 }
 
 type Health struct {
-	Status bool `json:"status"`
+	Status bool   `json:"status"`
+	Host   string `json:"hostname"`
 }
 
 func (health Health) status() int {
+	if health.Status != true {
+		return http.StatusInternalServerError
+	}
 	return http.StatusOK
 }
